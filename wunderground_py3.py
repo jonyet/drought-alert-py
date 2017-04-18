cce0c345fded77c9c70e99f0cc1830f1b8486d8f
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Air.local>
2017-04-17 22:40:49 -0400

    Updated the print syntax and update the formatting of the date printout to make it more readable

--- wunderground_py3.py
+++ wunderground_py3.py
@@ -2,17 +2,23 @@ from urllib.request import urlopen
 import json
 city = "Marietta"
 state = "GA"
-date = "20170406"
+date = "20170416"
 data = urlopen(f'http://api.wunderground.com/api/cdc9167d053abdfa/history_{date}/q/{state}/{city}.json')
 json_string = str(data.read(),'utf-8')
 parsed_json = json.loads(json_string)
 #print(json.dumps(parsed_json, indent=4, sort_keys=True)) # No need for, but i like the way it printed out so clearly
 meantempi = parsed_json['history']['dailysummary'][0]['meantempi']
 precipi = parsed_json['history']['dailysummary'][0]['precipi']
-# using ''' to print just so we can do multi-line statements. 
-string_to_print = f'''Date: {date}
-City: {city}
-Average Temperature: {meantempi} degrees Farenheit
-Total Rainfall: {precipi} inches'''
-print(string_to_print)
+
+#added to make date a little clearer. Could add dict to make month read as the actual name
+year = date[0:4]
+month = date[4:6]
+day = date[6:8]
+
+# changed month number to month name for readability
+month_names_dict = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July'}
+month_name = month_names_dict[month]
+
+# changed back to print statement and no variable using \n to make separate lines
+print(f"Date: {month_name} {day}, {year}\nCity: {city}\nAverage Temperature: {meantempi} degrees Farenheit\nTotal Rainfall: {precipi} inches")
 data.close()
